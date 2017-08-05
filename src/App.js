e607775d51a67913abb30bb43553a0e2ade2fb79
williamjones62 <williamjones62@gmail.com>
2017-08-05 17:06:35 -0400

    inline styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DottedBox from './dottedBox';
+import Box from './box';
 
 class App extends Component {
 
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <DottedBox />
+        <Box />
       </div>
     );
   }

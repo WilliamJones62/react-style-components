32cdfefa12fc97bcdebc0506f77c727ba9932757
williamjones62 <williamjones62@gmail.com>
2017-08-05 16:53:02 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DottedBox from './dottedBox';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <DottedBox />
+      </div>
+    );
+  }
+}
+export default App;
